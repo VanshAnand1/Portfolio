7132e9877bc62e367b603e4bcb8633650bf624ac
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2024-06-30 14:27:20 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,35 @@
+// ABOUT ME TABS
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// SIDEMENU
+
+// var sidemenu = document.getElementById("sidemenu");
+
+// function openmenu() {
+//     sidemenu.style.right = "0";
+// }
+
+// function closemenu() {
+//     sidemenu.style.right = "-200px";
+// }
+
+
+
+
